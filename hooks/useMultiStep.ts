ca540fbc5189f9fe2500f1546c0b8b78b47e8b74
Samkbe <samkbe@gmail.com>
2023-07-22 16:23:40 -0500

    Created multiStep hook

---
+++ hooks/useMultiStep.ts
@@ -0,0 +1,35 @@
+import { ReactElement, useState } from "react";
+
+export function useMultistep(steps: ReactElement[]) {
+  const [stepsIndex, setStepIndex] = useState(0);
+
+  function moveBack() {
+    setStepIndex((index) => {
+      if (index <= 0) {
+        return index;
+      }
+      return index - 1;
+    });
+  }
+
+  function moveForward() {
+    setStepIndex((index) => {
+      if (index >= steps.length - 1) {
+        return index;
+      }
+      return index - 1;
+    });
+  }
+
+  function moveTo(index: number) {
+    setStepIndex(index);
+  }
+
+  return {
+    stepsIndex,
+    currentStep: steps[stepsIndex],
+    moveBack,
+    moveForward,
+    moveTo,
+  };
+}
