c601acff0e0949365a2374dd515f2f9f83bb2c05
Samkbe <samkbe@gmail.com>
2023-07-24 12:49:55 -0500

    Cleaned up and added error handling for getKeywords API route

--- pages/api/getKeywords.ts
+++ pages/api/getKeywords.ts
@@ -1,32 +1,42 @@
 import type { NextApiResponse, NextApiRequest } from "next";
 import { GoogleAdsApi } from "google-ads-api";
-import { getSession } from "next-auth/react";
 import { getToken } from "next-auth/jwt";
 
+interface KeywordNextApiRequest extends NextApiRequest {
+  body: {
+    keyword: string;
+  };
+}
+
 export default async function handler(
-  req: NextApiRequest,
+  req: KeywordNextApiRequest,
   res: NextApiResponse
 ) {
-  const client = new GoogleAdsApi({
-    client_id: process.env.GOOGLE_CLIENT_ID as string,
-    client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
-    developer_token: process.env.GOOGLE_DEVELOPER_TOKEN as string,
-  });
-
+  if (req.method !== "GET") {
+    res.status(400).json("Only GET requests accepted");
+    res.end();
+  }
+  const { keyword } = req.body;
   const token = await getToken({ req });
 
   if (token) {
+    const client = new GoogleAdsApi({
+      client_id: process.env.GOOGLE_CLIENT_ID as string,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
+      developer_token: process.env.GOOGLE_DEVELOPER_TOKEN as string,
+    });
+
     const customer = client.Customer({
       customer_id: process.env.GOOGLE_CUSTOMER_ID as string,
       refresh_token: token?.refresh_token,
     });
 
     try {
-      customer.keywordPlanIdeas
-        .generateKeywordIdeas({
+      const keywordIdeas = await customer.keywordPlanIdeas.generateKeywordIdeas(
+        {
           customer_id: "7834575692",
           geo_target_constants: [],
-          page_size: 3,
+          page_size: 10,
           page_token: "",
           keyword_annotation: [],
           keyword_plan_network: "GOOGLE_SEARCH",
@@ -35,17 +45,19 @@ export default async function handler(
           },
           include_adult_keywords: false,
           keyword_seed: {
-            keywords: ["go to market agency"],
+            keywords: [keyword],
           },
-        })
-        .then((results) => res.status(200).json(results))
-        .catch((e) => {
-          console.log(e);
-        });
+        }
+      );
+      res.status(201).json(keywordIdeas);
+      res.end();
     } catch (e) {
       console.log(e);
       res.status(400).json(e);
+      res.end();
     }
+  } else {
+    res.status(400).json("Sorry, Something went wrong");
+    res.end();
   }
-  res.status(400).json("Sorry, there was no token");
 }
