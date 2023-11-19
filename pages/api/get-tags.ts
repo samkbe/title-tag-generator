0999bcdfd38f625994f3a8a8bd913d0763665323
Samkbe <samkbe@gmail.com>
2023-11-19 09:56:33 -0600

    Added functionality for fetching results from cohere

--- pages/api/get-tags.ts
+++ pages/api/get-tags.ts
@@ -3,7 +3,6 @@ import OpenAI from "openai";
 import {
   GetKeywordResults,
   GetTagsRequest,
-  GetMetadataResponse,
   GeneratedKeywordResponse,
 } from "../../types";
 
@@ -29,6 +28,14 @@ export default async function handler(
         })
       );
     } else if (llm === "cohere") {
+      res.status(200).json(
+        await getCohereResult({
+          keyword,
+          url,
+          companyName,
+          llm,
+        })
+      );
     } else if (llm === "fireworks") {
     } else {
       throw Error();
@@ -93,5 +100,53 @@ async function getOpenAiResults({
     };
   } else {
     throw new Error("Open API request error");
+  }
+}
+
+async function getCohereResult({
+  url,
+  keyword,
+  companyName,
+  llm,
+}: GetKeywordResults): Promise<GeneratedKeywordResponse> {
+  const prompt = `Act as an SEO expert. Generate an optimized title tag STRICTLY under 60 text characters and a description tag STRICTLY under 155 text characters with the keyword "${keyword}" for the company ${companyName}. Do not digress from the maximum character requirements. Their website is ${url}. Include the company name at the end of the title tag, not the beginning. Provide the title tag and description tag strictly in the following format without any other text:
+
+  Title Tag: [title]
+  Description Tag: [description]`;
+
+  const body = JSON.stringify({
+    prompt,
+    model: "command-nightly",
+    max_tokens: 300,
+    stop_sequences: [],
+    temperature: 0.1,
+    return_likelihoods: "NONE",
+    stream: false,
+  });
+
+  const response = await fetch(process.env.COHERE_API_ENDPOINT as string, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.COHERE_API_KEY}`,
+    },
+    body,
+  });
+
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    const text = jsonResponse.generations?.[0].text;
+    const trimmedText = text.trim();
+    const lines = trimmedText.split("\n");
+    const titleTag = lines[0].replace("Title Tag: ", "");
+    const descriptionTag = lines[1].replace("Description Tag: ", "");
+    return {
+      titleTag,
+      descriptionTag,
+      keyword,
+      llm,
+    };
+  } else {
+    throw new Error("Cohere API request error");
   }
 }
